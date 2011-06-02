d45c095462c9e0067a325cda63e6235497c0f040
Hedgehog <hedgehogshiatus@gmail.com>
2011-06-02 15:18:26 +1000

    dont' strip ansi colors.  Do this some other way.
    Signed-off-by: Hedgehog <hedgehogshiatus@gmail.com>

--- lib/cuken/api/common.rb
+++ lib/cuken/api/common.rb
@@ -20,25 +20,3 @@ module ::Cuken
   end
 end
 
-#
-# We strip ANSI code from output strings.
-#
-module ::Aruba
-  class Process
-    def remove_ansi_codes(str)
-      str.gsub(/\e\[(\d+)m/, '')
-    end
-    def stdout
-      wait_for_io do
-        @out.rewind
-        remove_ansi_codes(@out.read)
-      end
-    end
-    def stderr
-      wait_for_io do
-        @err.rewind
-        remove_ansi_codes(@err.read)
-      end
-    end
-  end
-end
